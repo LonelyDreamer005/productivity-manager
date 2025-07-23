a80fd6fbeadea91c78ddda194116d7f30ff8f02e
LonelyDreamer005 <vinay2020x15.10@gmail.com>
2025-07-23 23:12:36 +0530

    Basic setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+// server/index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('MongoDB connected');
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+})
+.catch((err) => console.error(err));
